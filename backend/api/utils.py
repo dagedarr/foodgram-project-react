76b69065068b2175b4421c5ee909f5ca9962b4f3
Natolin Artem <arnat64@yandex.ru>
2023-05-11 20:59:05 +0300

    added

---
+++ backend/api/utils.py
@@ -0,0 +1,20 @@
+from recipe.models import Component
+
+
+def ingredients_set(recipe, ingredients_data):
+    Component.objects.bulk_create([Component(
+        ingredient=ingredient['ingredient'],
+        recipe=recipe,
+        amount=ingredient['amount']
+    ) for ingredient in ingredients_data])
+
+
+def create_content(ingredients):
+    content = 'Ваш список покупок:\n'
+
+    for ingredient in ingredients:
+        content += (f"{ingredient['ingredient__name']} " +
+                    f"({ingredient['ingredient__measurement_unit']}) " +
+                    f"{ingredient['amount']}" +
+                    '\n')
+    return content
