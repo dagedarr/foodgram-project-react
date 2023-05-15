2cf229c3e060d63dc25be031564c7d91c2245317
Natolin Artem <arnat64@yandex.ru>
2023-05-15 13:08:13 +0300

    pep8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from users.views import UserViewSet
-
 
 app_name = 'api'
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
+from api.pagination import CustomPagination
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from api.pagination import CustomPagination
 from recipe.models import (Component, Favorite, Ingredient, Recipe,
                            ShoppingCart, Tag)
 from rest_framework import mixins, permissions, status, viewsets
